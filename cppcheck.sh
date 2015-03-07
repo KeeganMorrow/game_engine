116e0c89de7d9e89a9931f60f4fadd0db8ef1637
Korrow <Korrow@gmail.com>
2015-03-06 19:45:52 -0800

    Improvements to cppcheck script
    cppcheck now runs with the new structure and correctly recieves
    command line arguments given to the script. Also added libraries to
    its includes.

--- cppcheck.sh
+++ cppcheck.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-SRCDIR=$DIR"/src"
-INCDIR=$SRCDIR"/include/"
+SRCDIR="${DIR}/src"
+INCDIR="${DIR}/inc"
+EXINCDIR="${DIR}/lib/entityx"
+GTESTINCDIR="${DIR}/lib/gtest/include"
+GMOCKINCDIR="${DIR}/lib/gmock/include"
 echo "Running cppcheck on directory: \"$SRCDIR\""
-cppcheck --enable=all --inconclusive -I $INCDIR $SRCDIR 
+cppcheck --enable=all --inconclusive -I $INCDIR -I $EXINCDIR -I $GTESTINCDIR -I $GMOCKINCDIR $SRCDIR $@
