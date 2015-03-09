fd19a8e80297322eab252aadfe676d38a7483bcd
Korrow <Korrow@gmail.com>
2015-03-08 19:48:34 -0700

    Removed header dependencies that broke build

--- test/core/world.cpp
+++ test/core/world.cpp
@@ -1,8 +1,6 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 #include "core/world.hpp"
-#include "components/basic/position.hpp"
-#include "components/render/render.hpp"
 using namespace core;
 
 TEST(WorldTest, init) {
