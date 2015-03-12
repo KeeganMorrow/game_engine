e958e700ca12e8bcaf44f5261c1a5bf06595c09e
Korrow <Korrow@gmail.com>
2015-03-12 01:22:02 -0700

    Remove flag check from window test
    Removed checking the window flags from the window test. It was causing
    issues and on further consideration isn't really a valid check.

--- test/sdlwrap/window.cpp
+++ test/sdlwrap/window.cpp
@@ -9,13 +9,12 @@ using namespace sdlwrap;
 #ifndef NO_GUI
 
 TEST(sdlwrapWindow, Constructor){
-    Window w("test", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN);
+    Window w("test", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, 0);
     int x, y;
     w.GetSize(&x,&y);
     EXPECT_EQ(640, x);
     EXPECT_EQ(480, y);
     Uint32 flags = w.GetFlags();
-    EXPECT_EQ(0, SDL_WINDOW_SHOWN);
 }
 
 #endif //NO_GUI
