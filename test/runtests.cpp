7458d5658526097858eae2efef2cf42aefa8550f
Korrow <Korrow@gmail.com>
2015-03-18 23:45:57 -0700

    Remove log setup from tests
    Temporarily remove the logging configuration from the test running
    file until logging has been sorted out.

--- test/runtests.cpp
+++ test/runtests.cpp
@@ -2,9 +2,6 @@
 #include "core/logging/logging.hpp"
 
 int main(int argc, char **argv) {
-    logging::LoggerManager *pLogManager = new logging::LoggerManager();
-    pLogManager->loadConfig("test/logging.conf");
-    LOG(INFO) << "testing some logging";
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
