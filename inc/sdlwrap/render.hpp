dcf04e5e0bb8b3cdbdb43a0df0e3fc27ea5f6908
Korrow <Korrow@gmail.com>
2015-03-19 00:00:49 -0700

    Fix render test test on non hw accel'd platforms
    Made the render constructor flags in the test generic so that it
    would pass on platforms without hardware acceleration (like Travis).

--- inc/sdlwrap/render.hpp
+++ inc/sdlwrap/render.hpp
@@ -10,6 +10,7 @@ class Render{
     public:
         Render(Window *pwindow, int index, Uint32 flags):prenderer(nullptr){
             prenderer = SDL_CreateRenderer(pwindow->getWindow(), index, flags);
+            //TODO: Validate prenderer and throw exception if nullptr
         }
         ~Render(){
             assert(prenderer != nullptr);
--- src/sdlwrap/render.cpp
+++ src/sdlwrap/render.cpp
@@ -10,6 +10,6 @@ using namespace sdlwrap;
 TEST(sdlwrapRender, constructor) {
     SDL_Init(SDL_INIT_EVERYTHING);
     Window w("test", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, 0);
-    Render r(&w, -1, SDL_RENDERER_ACCELERATED);
+    Render r(&w, -1, 0 );
     SDL_Quit();
 }
