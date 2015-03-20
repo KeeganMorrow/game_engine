f2af051acfa346c9638a65be3fe600efb42418b5
Korrow <Korrow@gmail.com>
2015-03-19 22:30:22 -0700

    Fixed swapped render implementation and test
    The implementation and test source files for the render class in
    sdlwrap were swapped. D'oh!

--- test/sdlwrap/render.cpp
+++ test/sdlwrap/render.cpp
@@ -1,1 +1,15 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include <SDL2/SDL.h>
+
+#include "sdlwrap/window.hpp"
 #include "sdlwrap/render.hpp"
+
+using namespace sdlwrap;
+
+TEST(sdlwrapRender, constructor) {
+    SDL_Init(SDL_INIT_EVERYTHING);
+    Window w("test", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, 0);
+    Render r(&w, -1, 0 );
+    SDL_Quit();
+}
