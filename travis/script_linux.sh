37ab2872655adf5951507c42e5da049cc25e2418
Korrow <Korrow@gmail.com>
2015-10-03 16:57:33 -0700

    Travis: Yet another fix

--- travis/script_linux.sh
+++ travis/script_linux.sh
@@ -3,7 +3,7 @@ echo "########################################################################"
 echo "# Travix Linux Script"
 echo "########################################################################"
 cd build
-cmake -Dsdlwrap_test_no_graphics ..
+cmake -Dsdlwrap_test_no_graphics=ON ..
 make
 CTEST_OUTPUT_ON_FAILURE=1 make test
 ./test/test_sdlwrap
