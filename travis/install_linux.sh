48c843e060bcec2dc185278ceb8efd30e8e55dce
Korrow <Korrow@gmail.com>
2015-10-03 16:32:00 -0700

    Travis: Improve sudo usage

--- travis/install_linux.sh
+++ travis/install_linux.sh
@@ -4,8 +4,8 @@ echo "########################################################################"
 echo "# Travix Linux Install Script"
 echo "########################################################################"
 
-sudo apt-get install -qq libsdl2-dev libsdl2-image-dev liblog4cplus-dev gcc-4.8 g++-4.8
-sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 90
+apt-get install -qq libsdl2-dev libsdl2-image-dev liblog4cplus-dev gcc-4.8 g++-4.8
+update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 90
 wget http://www.cmake.org/files/v3.3/cmake-3.3.1-Linux-x86_64.tar.gz -O cmake.tar.gz
 tar -xzf cmake.tar.gz -C $HOME
 export PATH=$HOME/cmake-3.3.1-Linux-x86_64/bin:$PATH
