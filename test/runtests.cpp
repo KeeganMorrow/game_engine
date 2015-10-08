8eb2cf4e02fccfec45889faa594c311a8b0ccbc0
Korrow <Korrow@gmail.com>
2015-10-07 21:54:15 -0700

    test: Add logging module macros to test runner file
    Add logging module macros to the test runner file to
    enable logging during tests.

--- test/runtests.cpp
+++ test/runtests.cpp
@@ -1,6 +1,10 @@
 #include "gtest/gtest.h"
+#include "core/logging.hpp"
+
+INITIALIZE_EASYLOGGINGPP
 
 int main(int argc, char **argv) {
+    START_EASYLOGGINGPP(argc, argv);
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
